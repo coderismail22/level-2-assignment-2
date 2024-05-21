6d5c918d84b8fbd558f9ba6596186d9fe1166b22
ismail <coderismail22@gmail.com>
2024-05-21 21:32:11 +0600

    check conditionally getOrdersByEmail and getAllOrders

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -3,7 +3,20 @@ import { orderControllers } from './order.controller';
 import { request } from 'http';
 const router = express.Router();
 
+interface EmailQuery extends Request {
+  query: {
+    email?: string;
+  };
+}
 
+//to maintain the same api endpoint
+const email = (req: EmailQuery, res: Response) => {
+  if (req.query.email) {
+    return router.get('/', orderControllers.getOrdersByEmail);
+  } else {
+    return router.get('/', orderControllers.getAllOrders);
+  }
+};
 
 router.post('/', orderControllers.createNewOrder);
 
