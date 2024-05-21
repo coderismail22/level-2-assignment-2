4f7f9fa39177eb57150637d7c2de3b9edb2d42eb
ismail <coderismail22@gmail.com>
2024-05-21 21:11:34 +0600

    create getAllOrders route

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -3,5 +3,6 @@ import { orderControllers } from './order.controller';
 const router = express.Router();
 
 router.post('/', orderControllers.createNewOrder);
+router.get('/', orderControllers.getAllOrders);
 
 export const OrderRoutes = router;
