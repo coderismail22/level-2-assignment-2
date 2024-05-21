68c737af65fdd26c9e0635c04b351b897f7668be
ismail <coderismail22@gmail.com>
2024-05-21 07:23:42 +0600

    create-insertNewProduct-controller

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -1,7 +1,21 @@
+import { Request, Response } from 'express';
+import { productServices } from './product.service';
+
 const insertNewProduct = async (req: Request, res: Response) => {
   try {
     //call insertNewProduct services
+    const { product } = req.body;
+    const result = await productServices.insertNewProduct(product);
+    res.send(200).json({
+      success: true,
+      message: 'Inserted product successfully',
+      result: result,
+    });
   } catch (error) {
     console.log(error);
   }
+};
+
+export const productController = {
+  insertNewProduct,
 };
