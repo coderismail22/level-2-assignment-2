8af7c359624e6a9efebd036141dc4cc25ca34d90
ismail <coderismail22@gmail.com>
2024-05-21 08:03:15 +0600

    add new product post route

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import { productControllers } from './product.controller';
 const router = express.Router();
 
-
+router.post('/', productControllers.insertNewProduct);
 
 export const StudentRoutes = router;
