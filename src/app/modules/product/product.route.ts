c78635813a9eb4131c859938eecbdc93e7ebafc8
ismail <coderismail22@gmail.com>
2024-05-21 14:21:35 +0600

    use same end point to get different results based on query

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -1,9 +1,29 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { productControllers } from './product.controller';
 const router = express.Router();
 
+// Define the type for the query parameters for extra type safety
+interface ProductQuery extends Request {
+  query: {
+    searchTerm?: string;
+  };
+}
+
+//to maintain the same api endpoint
+const checkSearchTerm = (req: ProductQuery, res: Response) => {
+  if (req.query.searchTerm) {
+    return productControllers.getProductBySearchQuery(req, res);
+  } else {
+    return productControllers.getAllProducts(req, res);
+  }
+};
+
 router.post('/', productControllers.insertNewProduct);
-router.get('/', productControllers.getAllProducts);
 router.get('/:productId', productControllers.getSingleProduct);
+// router.get('/', productControllers.getAllProducts);
+// router.get('/', productControllers.getProductBySearchQuery);
+
+//to maintain the same api endpoint
+router.get('/', checkSearchTerm);
 
 export const ProductRoutes = router;
