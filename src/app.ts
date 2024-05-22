82be158a6b597862e1b0546b6c34591b8f50febd
ismail <coderismail22@gmail.com>
2024-05-22 18:42:37 +0600

    add not found route middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { ProductRoutes } from './app/modules/product/product.route';
 import { OrderRoutes } from './app/modules/order/order.route';
@@ -12,6 +12,14 @@ app.use(cors());
 //application routes
 app.use('/api/products', ProductRoutes);
 app.use('/api/orders', OrderRoutes);
+
+// Middleware for handling 404 errors
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
 
 //default
 app.get('/', (req: Request, res: Response) => {
